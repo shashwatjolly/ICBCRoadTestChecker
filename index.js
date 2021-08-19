5ef6dc7b169fdb390397bfa3f925f9d95517998c
Shashwat Jolly <shashat.jolly@gmail.com>
2021-08-18 22:55:10 -0700

    Slow down script execution and clean up output

--- index.js
+++ index.js
@@ -31,39 +31,34 @@ const checkAvailabliityInContainer = async (page, container) => {
     await container.click();
     const elementHandle = await page.waitForSelector('.no-appts-msg, .date-title', {timeout: 5000});
     const className = await page.evaluate(element => element.className, elementHandle);
-    console.log("WOOHOO " + className)
-    const temp = await page.evaluate(element => element.innerHTML, elementHandle);
-    console.log("WOOHOOOOOO " + temp)
+    const title = await page.$eval('.location-title', text => text.innerHTML);
+    console.log(title + ":");
     if(className != "no-appts-msg") {
-        const t = await page.$$eval('button.mat-button-toggle-button', buttons => buttons.map(button => button.innerHTML));
+        // TODO: logic for parsing dates and times
+        const t = await page.$$eval('.date-title, div.mat-button-toggle-label-content', div => div.map(button => button.innerHTML));
         console.log(t);
+    }
+    else {
+        console.log("No slots available");
     }
     await page.$eval('div.actions-container > button.mat-stroked-button', button => button.click());
 }
 
 const searchAppointmentsByLocation = async (page, location) => {
     await page.type('input[formcontrolname="finishedAutocomplete"]', location);
-    await page.waitForSelector("mat-option", {timeout: 5000});
+    await page.waitForSelector("mat-option:first-of-type", {timeout: 5000});
     await page.$eval('mat-option:first-of-type', option => option.click());
     console.log("Selected " + location + " as the location");
     await page.$eval('button.mat-raised-button', button => button.click());
     await page.waitForSelector("div.department-container", {timeout: 5000});
     const containers = await page.$$('div.department-container');
-    // console.log(containers.length);
-    for(let i = 0; i < containers.length; i+=1) {
+    for(let i = 0; i < containers.length; i++) {
         await checkAvailabliityInContainer(page, containers[i]);
     }
-    // containers.forEach(async (container) => {
-        // const t = await page.evaluate(container => container.innerHTML, container);
-        // console.log(t);
-        // await checkAvailabliityInContainer(page, container);
-    // }); 
-    // const containers = await page.$$('div.department-container');
-    // await checkAvailabliityInContainer(page, containers[1]);
 }
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({slowMo: 250 });
     const page = await browser.newPage();
     await signIn(browser, page);
     await rescheduleAppointment(page);
