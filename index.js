456bc2d5ed53f2bd043018239600ddd92cc15a91
Shashwat Jolly <shashat.jolly@gmail.com>
2021-08-22 02:18:23 -0700

    Implement email functionality for notifications

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const request = require('request');
 
 const signIn = async (browser, page) => {
     console.log("Opening ICBC's website...")
@@ -20,7 +21,7 @@ const signIn = async (browser, page) => {
     // await browser.close();
 }
 
-const rescheduleAppointment = async (page) => {
+const rescheduleAppointment = async (page, location, validAvailableDates) => {
     const appointmentDate = (await page.$eval('div.appointment-time', div => div.innerHTML)).split('<')[0];
     const parts = appointmentDate.trim().split(',');
     currentAppointmentDate = Date.parse(parts[0].slice(0, -2) + "," + parts[1]);
@@ -28,17 +29,16 @@ const rescheduleAppointment = async (page) => {
     await page.$eval('div.otp-action-buttons > button.mat-raised-button', button => button.click());
     await page.waitForSelector("button.search-button", {timeout: 5000});
     console.log("Moving on to location search...");
-    await searchAppointmentsByLocation(page, "Whistler, BC");
+    await searchAppointmentsByLocation(page, location, validAvailableDates);
 }
 
-const checkAvailabliityInContainer = async (page, container) => {
+const checkAvailabliityInContainer = async (page, container, validAvailableDates) => {
     await container.click();
     const elementHandle = await page.waitForSelector('.no-appts-msg, .date-title', {timeout: 5000});
     const className = await page.evaluate(element => element.className, elementHandle);
     const title = await page.$eval('.location-title', text => text.innerHTML);
     console.log(title + ":");
     if(className != "no-appts-msg") {
-        // TODO: logic for parsing dates and times
         const dates = await page.$$eval('.date-title', div => div.map(button => button.innerHTML));
         const parsedDates = dates.map(date => {
                                 const parts = date.trim().split(',');
@@ -46,12 +46,12 @@ const checkAvailabliityInContainer = async (page, container) => {
                                 return Date.parse(formattedDate);
                             });
         currentAppointmentDate = Date.parse("February 9, 2022");
-        let validAvailableDates = parsedDates.filter(parsedDate => {
+        validAvailableDates[title] = parsedDates.filter(parsedDate => {
             const daysDifferenceAppointment = Math.ceil((currentAppointmentDate - parsedDate)/(1000 * 60 * 60 * 24));
             const daysDifferenceCurrent = Math.floor((parsedDate - Date.now())/(1000 * 60 * 60 * 24));
             return daysDifferenceAppointment > 0 && daysDifferenceCurrent >= 30;
         });
-        console.log(validAvailableDates.map(date => (new Date(date)).toDateString()));
+        validAvailableDates[title] = validAvailableDates[title].map(date => (new Date(date)).toDateString());
     }
     else {
         console.log("No slots available");
@@ -59,7 +59,7 @@ const checkAvailabliityInContainer = async (page, container) => {
     await page.$eval('div.actions-container > button.mat-stroked-button', button => button.click());
 }
 
-const searchAppointmentsByLocation = async (page, location) => {
+const searchAppointmentsByLocation = async (page, location, validAvailableDates) => {
     await page.type('input[formcontrolname="finishedAutocomplete"]', location);
     await page.waitForSelector("mat-option:first-of-type", {timeout: 5000});
     await page.$eval('mat-option:first-of-type', option => option.click());
@@ -68,8 +68,49 @@ const searchAppointmentsByLocation = async (page, location) => {
     await page.waitForSelector("div.department-container", {timeout: 5000});
     const containers = await page.$$('div.department-container');
     for(let i = 0; i < containers.length; i++) {
-        await checkAvailabliityInContainer(page, containers[i]);
+        await checkAvailabliityInContainer(page, containers[i], validAvailableDates);
     }
+}
+
+const sendNotificationEmail = (validAvailableDates) => {
+    let emailBody = "";
+    for(location in validAvailableDates) {
+        let emailBodyForLocation = ""
+        for(centre in validAvailableDates[location]) {
+            if(validAvailableDates[location][centre].length == 0) {
+                continue;
+            }
+            emailBodyForLocation += "<h3>" + centre + "</h3> </br> <ul>";
+            for(date of validAvailableDates[location][centre]) {
+                emailBodyForLocation += "<li>" + date + "</li>";
+            }
+            emailBodyForLocation += "</ul><br>";
+        }
+        if(emailBodyForLocation.length > 0) {
+            emailBody += "<h2>" + location + ":</h2> </br>" + emailBodyForLocation;
+        }
+    }
+
+    if(emailBody.length == 0) {
+        return;
+    }
+
+    const options = {
+        url: 'https://icbcroadtestchecker.azurewebsites.net/api/sendNotificationEmail?code=kw84GIMWVRrQOxfVUYksizytI1igUe7uUhVgrxjIEk80i1eclHn47w==',
+        json: true,
+        body: {
+            userEmail: "jahnvi.1210@gmail.com",
+            emailBody
+        }
+    };
+    
+    request.post(options, (err, res, body) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(`Status: ${res.statusCode}`);
+        console.log(body);
+    });
 }
 
 let currentAppointmentDate = "";
@@ -77,6 +118,15 @@ let currentAppointmentDate = "";
 (async () => {
     const browser = await puppeteer.launch({slowMo: 150});
     const page = await browser.newPage();
-    await signIn(browser, page);
-    await rescheduleAppointment(page);
+    let validAvailableDates = {};
+    let validLocations = ["Vancouver, BC", "Richmond, BC"];
+    for(let i = 0; i<validLocations.length; i++) {
+        validAvailableDates[validLocations[i]] = {};
+        await signIn(browser, page);
+        await rescheduleAppointment(page, validLocations[i], validAvailableDates[validLocations[i]]);
+    }
+    console.log(validAvailableDates);
+    sendNotificationEmail(validAvailableDates);
+    await browser.close();
 })();
+
